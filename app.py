b51d89e6e05c105b6b70037a0746ee96abfb724e
bsmoon <45582617+bsmoon-86@users.noreply.github.com>
2025-04-07 00:56:48 +0900

    update

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    df = pd.read_csv("./csv/iris.csv")
+    data = df.to_dict(orient='records')
+    cols = df.columns
+    return render_template('dashboard.html', table_data = data, table_cols = cols)
+
+@app.route('/open_api')
+def open():
+    serviece_key = request.args['servicekey']
+    if serviece_key == 'asdf1234':
+        df = pd.read_csv("./csv/df_sample.csv")
+        result = df.to_dict(orient='records')
+        return result
+    else:
+        return "Servicekey_error"
+
+
+app.run()
